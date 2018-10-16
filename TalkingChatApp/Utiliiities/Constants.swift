84040d23d09a18ce6e621f858df81f7a5f21ddf3
Sasa <brezovac.sasa@gmail.com>
2018-10-16 17:21:38 +0200

    authService, userRegister

--- TalkingChatApp/Utiliiities/Constants.swift
+++ TalkingChatApp/Utiliiities/Constants.swift
@@ -8,10 +8,24 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+
+// URL Constants
+let BASE_URL = "https://talkingchat.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+
 //Segues
 let TO_LOGIN = "toLogin"
 let CREATE_AN_ACCOUNT = "create_an_account"
 let UNWIND = "unwindToChannel"
+
+
+//UserDefaults
+let TOKEN_KEY = "token"
+let IS_LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
 
 
 
