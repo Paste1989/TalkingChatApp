5e80157c2f854c7c4d28f1bc4b8e233caeea192e
Sasa <brezovac.sasa@gmail.com>
2018-10-27 19:03:57 +0200

    fetchingMessages

---
+++ TalkingChatApp/Model/Message.swift
@@ -0,0 +1,20 @@
+//
+//  Message.swift
+//  TalkingChatApp
+//
+//  Created by Saša Brezovac on 27.10.2018..
+//  Copyright © 2018. CopyPaste89. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    
+    public private(set) var message: String!
+    public private(set) var userName: String!
+    public private(set) var channelId: String!
+    public private(set) var userAvatar: String!
+    public private(set) var userAvatarColor: String!
+    public private(set) var id: String!
+    public private(set) var timeStamp: String!
+}
